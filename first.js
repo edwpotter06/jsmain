b84068d3c16f3fd42e1dc789ab41bb4c9dba491b
edwpotter06 <137563057+edwpotter06@users.noreply.github.com>
2024-03-12 00:16:08 +0300

    Update first.js

--- first.js
+++ first.js
@@ -2,3 +2,11 @@ const slider = document.querySelector('.gallery');
 let isDown = false;
 let startX;
 let scrollLeft;
+
+
+slider.addEventListener('mousedown', e => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
