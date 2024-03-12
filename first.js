09527a1c1df3a66427dad2427d87ecbef07c3762
edwpotter06 <137563057+edwpotter06@users.noreply.github.com>
2024-03-12 12:18:04 +0300

    Update first.js

--- first.js
+++ first.js
@@ -10,3 +10,20 @@ slider.addEventListener('mousedown', e => {
   startX = e.pageX - slider.offsetLeft;
   scrollLeft = slider.scrollLeft;
 });
+
+slider.addEventListener('mouseleave', _ => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', _ => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', e => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const SCROLL_SPEED = 3;
+  const walk = (x - startX) * SCROLL_SPEED;
+  slider.scrollLeft = scrollLeft - walk;
+});
